79bfb6a1c943836a0184f23a7f8d76c5d25619b7
Shane Foley <shanefoley@Shanes-MacBook-Pro.local>
2019-11-24 20:19:19 +0100

    .js files to add charts and pie chart

---
+++ githubapi/src/components/Charts.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Bar} from 'react-chartjs-2';
+
+
+export default class Chart extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            chartData:props.chartData
+
+        }
+    }
+    render(){
+        return(
+          
+        )
+    }
+}
---
+++ githubapi/src/components/PieChart.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Pie} from 'react-chartjs-2';
+
+
+export default class PieChart extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            pieChartData:props.pieChartData
+
+        }
+    }
+    render(){
+        return(
+            
+        )
+    }
+}
